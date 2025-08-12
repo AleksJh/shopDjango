9fb7cb567f2dd7f4149a285a6c25b5f0e1e079de
Aleksan.Jhangiryan <lernagorc90@gmail.com>
2025-08-12 15:52:42 +0400

    chore: Apply linting and formatting

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -1,6 +1,8 @@
 from decimal import Decimal
+
 from django.conf import settings
 from shop.models import Product
+
 
 class Cart:
     def __init__(self, request):
@@ -11,24 +13,23 @@ class Cart:
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
 
-    def add(self, product, quantity=1,  override_quantity=False):
-        """ Add a product to the cart or update it. """
+    def add(self, product, quantity=1, override_quantity=False):
+        """Add a product to the cart or update it."""
         product_id = str(product.id)
         if product_id not in self.cart:
-            self.cart[product_id] = {'quantity': 0,
-                                     'price': str(product.price)}
+            self.cart[product_id] = {"quantity": 0, "price": str(product.price)}
         if override_quantity:
-            self.cart[product_id]['quantity'] = quantity
+            self.cart[product_id]["quantity"] = quantity
         else:
-            self.cart[product_id]['quantity'] += quantity
+            self.cart[product_id]["quantity"] += quantity
         self.save()
 
     def save(self):
-        """ Mark the session as modified to save changes. """
+        """Mark the session as modified to save changes."""
         self.session.modified = True
 
     def remove(self, product):
-        """ Remove the product from the cart. """
+        """Remove the product from the cart."""
         product_id = str(product.id)
         if product_id in self.cart:
             del self.cart[product_id]
@@ -40,18 +41,20 @@ class Cart:
         products = Product.objects.filter(id__in=product_ids)
         cart = self.cart.copy()
         for product in products:
-            cart[str(product.id)]['product'] = product
+            cart[str(product.id)]["product"] = product
         for item in cart.values():
-            item['price'] = Decimal(item['price'])
-            item['total_price'] = item['price'] * item['quantity']
+            item["price"] = Decimal(item["price"])
+            item["total_price"] = item["price"] * item["quantity"]
             yield item
 
     def __len__(self):
-        """ Count all product positions in the cart. """
-        return sum(item['quantity'] for item in self.cart.values())
+        """Count all product positions in the cart."""
+        return sum(item["quantity"] for item in self.cart.values())
 
     def get_total_price(self):
-        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
+        return sum(
+            Decimal(item["price"]) * item["quantity"] for item in self.cart.values()
+        )
 
     def clear(self):
         del self.session[settings.CART_SESSION_ID]
--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,7 +1,7 @@
+from cart.forms import CartAddProductForm
 from django.shortcuts import get_object_or_404, render
 
 from .models import Category, Product
-from cart.forms import CartAddProductForm
 
 
 def product_list(request, category_slug=None):
@@ -21,7 +21,8 @@ def product_list(request, category_slug=None):
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
     cart_product_form = CartAddProductForm()
-    return render(request, "shop/product/detail.html",
-                  {"product": product,
-                   'cart_product_form': cart_product_form})
-
+    return render(
+        request,
+        "shop/product/detail.html",
+        {"product": product, "cart_product_form": cart_product_form},
+    )
