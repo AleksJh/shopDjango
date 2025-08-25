7018e839e22309ecd238922bd6ad02c846a79bf7
Aleksan.Jhangiryan <lernagorc90@gmail.com>
2025-08-25 19:02:17 +0400

    chore: Apply linting and formatting

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -1,29 +1,38 @@
 import csv
 import datetime
+
+from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import reverse
-from django.contrib import admin
 from django.utils.safestring import mark_safe
 
 from .models import Order, OrderItem
 
+
 def order_pdf(obj):
-    url = reverse('orders:admin_order_pdf', args=[obj.id])
+    url = reverse("orders:admin_order_pdf", args=[obj.id])
     return mark_safe(f'<a href="{url}">PDF</a>')
-order_pdf.short_description = 'Invoice'
+
+
+order_pdf.short_description = "Invoice"
+
 
 def order_detail(obj):
-    url = reverse('orders:admin_order_detail', args=[obj.id])
+    url = reverse("orders:admin_order_detail", args=[obj.id])
     return mark_safe(f'<a href="{url}">View</a>')
+
 
 def export_to_csv(modeladmin, request, queryset):
     opts = modeladmin.model._meta
-    content_disposition = f'attachment; filename={opts.verbose_name}.csv'
-    response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = content_disposition
-    writer =  csv.writer(response)
-    fields = [field for field in opts.get_fields() if not \
-              field.many_to_many and not field.one_to_many]
+    content_disposition = f"attachment; filename={opts.verbose_name}.csv"
+    response = HttpResponse(content_type="text/csv")
+    response["Content-Disposition"] = content_disposition
+    writer = csv.writer(response)
+    fields = [
+        field
+        for field in opts.get_fields()
+        if not field.many_to_many and not field.one_to_many
+    ]
     # write the first line with header information
     writer.writerow([field.verbose_name for field in fields])
     # write data lines
@@ -32,11 +41,14 @@ def export_to_csv(modeladmin, request, queryset):
         for field in fields:
             value = getattr(obj, field.name)
             if isinstance(value, datetime.datetime):
-                value = value.strftime('%d/%m/%Y')
+                value = value.strftime("%d/%m/%Y")
             data_row.append(value)
         writer.writerow(data_row)
     return response
-export_to_csv.short_description = 'Export to CSV'
+
+
+export_to_csv.short_description = "Export to CSV"
+
 
 def order_stripe_payment(obj):
     url = obj.get_stripe_url()
